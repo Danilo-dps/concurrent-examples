696316e7fbeb3c92f2624e5823cd23f018b6bdea
Danilo-dps <danilodpsnilo@outlook.com>
2026-02-01 18:56:12 -0300

    feat: projeto iniciado

---
+++ src/test/java/com/danilodps/concurrent/ConcurrentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilodps.concurrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
